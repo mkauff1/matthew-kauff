f2cf31542c613024dea7cdd79a1107debc178932
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-24 18:30:31 -0400

    add more changes

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict";
+const logoIntro = document.querySelector(".logo-intro");
 
 var typed = new Typed(".typing",{
-    strings:["Full Stack Web Developer","Graphic Designer","Codeup student"],
+    strings:["Full Stack Web Developer ","Graphic Designer ","Codeup student " ],
     typeSpeed:120,
     BackSpeed:40,
     loop:true
+})
+
+logoIntro.addEventListener('click', () =>{
+    document.body.classList.toggle("intro-refresh")
 })
