25202104780887f69637cb414f2762b75f5e1870
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-24 16:46:24 -0400

    add animation and updated css

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var typed = new Typed(".typing",{
+    strings:["Full Stack Web Developer","Graphic Designer","Codeup student"],
+    typeSpeed:120,
+    BackSpeed:40,
+    loop:true
+})
