dd98c860aaa4877817b3027e94f535d67a24cd1f
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-28 15:00:28 -0400

    add preloader

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ var typed = new Typed(".typing",{
 logoIntro.addEventListener('click', () =>{
     document.body.classList.toggle("intro-refresh")
 })
+
+//var loader = document.getElementById("preloader");
+
+//window.addEventListener("load", function() {
+//    loader.style.display = "none";
+//});
+
+logoJ(){
+    var loader = document.getElementById("preloader");
+    var loading = 0;
+    var id = setInterval(loads, 20);
+
+    function loads() {
+        if (loading == 0) {
+            loader.classList.add('active');
+        } else if (loading == 100) {
+            clearInterval(id);
+            window.open("index.html", "_self");
+            loader.classList.remove('active');
+        } else {
+            loading = loading + 1;
+            if (loading == 90) {
+                loader.style.animation = "fadeout 1s ease";
+            }
+        }
+    }
+};
+
